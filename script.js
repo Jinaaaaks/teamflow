fd1adbe34fa5cd4ffce2374f825ad2b3368f7f4b
Jinalee <jinaleesubasinghe16@gmail.com>
2026-01-02 13:58:57 +0530

    Add task input and task list functionality

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("TeamFlow loaded");
+const taskInput = document.getElementById("taskTitle");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+
+addTaskBtn.addEventListener("click", () => {
+  const taskText = taskInput.value.trim();
+
+  if (taskText === "") {
+    alert("Please enter a task");
+    return;
+  }
+
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  taskList.appendChild(li);
+  taskInput.value = "";
+});
